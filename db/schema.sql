0be9cf7fbbe246e58e1f470c583f57967d7a90d5
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-10-01 11:47:17 -0700

    trying to serve main page w/o auth

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS sobczyk_db;
+CREATE DATABASE sobczyk_db;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -11,7 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: '',
@@ -23,17 +23,17 @@ const sess = {
   })
 };
 
-app.use(session(sess));
+// app.use(session(sess));
 
-app.engine('handlebars', hbs.engine);
+// app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/css', express.static(path.join(_dirname, 'node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(_dirname, 'node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(_dirname, 'node_modules/jquery/dist')))
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 
 app.use(routes);
 
