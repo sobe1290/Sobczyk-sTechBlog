74b9cd372f58053839f19e03abb53dd43bdb1511
Patrick Sobczyk <sobczyk1290@gmail.com>
2022-10-01 22:41:19 -0700

    adding homepage postdata

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { raw } = require('express');
+const { User, Post } = require('../models');
 const withAuth = require('../utils/auth');
 
 // router.get('/', withAuth, async (req, res) => {
@@ -21,7 +22,16 @@ const withAuth = require('../utils/auth');
 // });
 
 router.get('/', async (req, res) => {
-  res.render('homepage');
+  try{ 
+    const postData = await Post.findAll().catch((err) => { 
+      res.json(err);
+    });
+      const posts = postData.map((post) => post.get({ plain: true }));
+      res.render('homepage', { posts });
+  } catch (err) {
+      res.status(500).json(err);
+  };   
+ 
 });
 
 router.get('/login', (req, res) => {
